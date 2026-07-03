199ec6474a3383d2e13f95445adc9599badc8a1f
zbecker <zebfullerbecker@gmail.com>
2026-07-03 00:38:45 +0000

    Try again

--- dps_run.sh
+++ dps_run.sh
@@ -2,7 +2,13 @@
 set -euo pipefail
 # Entrypoint script used to trigger NIFC scraping job on DPS 
 
-pwd
-ls
-ls fire-utils/
+basedir=$( cd "$(dirname "$0")"; pwd -P )
+echo "Basedir: $basedir"
+echo "Initial working directory: $(pwd -P)"
+echo "conda: $(which conda)"
+echo "Python: $(which python)"
+
+pushd "$basedir"
+echo "Running in directory: $(pwd -P)"
+
 conda run --live-stream --name scraper python scrape_nifc_perims.py
